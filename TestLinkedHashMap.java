50b8524cc192816aad216d52b2ae9a5913dadbac
Dwika Dyananda <101663374+DwikaDyananda@users.noreply.github.com>
2022-03-16 22:39:01 +0800

    Add files via upload

---
+++ TestLinkedHashMap.java
@@ -0,0 +1,21 @@
+package com.dicoding.javafundamental.collection;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class TestLinkedHashMap {
+
+    public static void main(String[] args){
+        //Dengan LinkedHashMap (output sesuai urutan input)
+        Map<String, Mahasiswa> daftarmhs = new LinkedHashMap<>();
+        daftarmhs.put("210555", new Mahasiswa("Arya", 19));
+        daftarmhs.put("210551", new Mahasiswa("Budi", 18));
+        daftarmhs.put("210553", new Mahasiswa("Juan", 18));
+        daftarmhs.put("210552", new Mahasiswa("Eka", 19));
+        daftarmhs.put("210554", new Mahasiswa("Monica", 18));
+
+        System.out.println(daftarmhs);
+
+    }
+}
+
